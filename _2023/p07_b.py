b15cc621b7a95b186feecdbde38a63ef406310d0
benb10 <46840126+benb10@users.noreply.github.com>
2024-11-24 20:06:10 +1000

    wip

--- _2023/p07_b.py
+++ _2023/p07_b.py
@@ -1,3 +1,89 @@
+from collections import Counter
+
+
+def get_hand_type_score(cards: str) -> int:
+    num_js = sum(1 for card in cards if card == "J")
+    non_j_cards = [card for card in cards if card != "J"]
+    counter = Counter(non_j_cards)
+    counts = sorted(counter.values())
+
+    # use Js to increase the best count
+    if counts:
+        counts[-1] += num_js
+    else:
+        counts = [num_js]
+
+    is_5oac = counts == [5]
+    if is_5oac:
+        return 7
+
+    is_4oac = counts == [1, 4]
+    if is_4oac:
+        return 6
+
+    is_full_house = counts == [2, 3]
+    if is_full_house:
+        return 5
+
+    is_3oac = counts == [1, 1, 3]
+    if is_3oac:
+        return 4
+
+    is_two_pair = counts == [1, 2, 2]
+    if is_two_pair:
+        return 3
+
+    is_one_pair = counts == [1, 1, 1, 2]
+    if is_one_pair:
+        return 2
+
+    is_high_card = counts == [1, 1, 1, 1, 1]
+    if is_high_card:
+        return 1
+
+    raise ValueError(cards)
+
+
+def get_card_score(card: str) -> int:
+    card_to_score = {
+        "J": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "T": 10,
+        "Q": 12,
+        "K": 13,
+        "A": 14,
+    }
+    return card_to_score[card]
+
+
+def get_hand_sort_key(hand: tuple[str, int]) -> tuple[int, ...]:
+    cards, _ = hand
+    hand_type_score = get_hand_type_score(cards)
+
+    card_scores = [get_card_score(card) for card in cards]
+
+    return hand_type_score, *card_scores
+
+
 def run(s: str) -> int:
-    # lines = [line.strip() for line in s.strip().split("\n")]
-    return 0
+    lines = [line.strip() for line in s.strip().split("\n")]
+    hands = []
+
+    for line in lines:
+        cards, num_str = line.split(" ")
+        hands.append((cards, int(num_str)))
+
+    hands.sort(key=get_hand_sort_key)
+    print(hands)
+    total = 0
+    for i, (_, num) in enumerate(hands, 1):
+        total += i * num
+
+    return total
--- _2023/p07_test.py
+++ _2023/p07_test.py
@@ -1,11 +1,8 @@
 from pathlib import Path
 
-import pytest
-
 from . import p07_a, p07_b
 
 
-@pytest.mark.skip("solution not complete")
 def test_p07a_small():
     s = """32T3K 765
     T55J5 684
@@ -16,22 +13,23 @@ def test_p07a_small():
     assert p07_a.run(s) == 6440
 
 
-@pytest.mark.skip("solution not complete")
 def test_p07a():
     s = (Path(__file__).parent / "p07_input.txt").read_text()
 
-    assert p07_a.run(s) == 1
+    assert p07_a.run(s) == 253603890
 
 
-@pytest.mark.skip("solution not complete")
 def test_p07b_small():
-    s = """"""
+    s = """32T3K 765
+    T55J5 684
+    KK677 28
+    KTJJT 220
+    QQQJA 483"""
 
-    assert p07_b.run(s) == 1
+    assert p07_b.run(s) == 5905
 
 
-@pytest.mark.skip("solution not complete")
 def test_p07b():
     s = (Path(__file__).parent / "p07_input.txt").read_text()
 
-    assert p07_b.run(s) == 1
+    assert p07_b.run(s) == 253630098
