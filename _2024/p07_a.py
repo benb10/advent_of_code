fbf3dc1643ac37ce4e70da9cb2ec2bd7e3ef9663
benb10 <46840126+benb10@users.noreply.github.com>
2024-12-07 16:10:53 +1000

    p7

---
+++ _2024/p07_a.py
@@ -0,0 +1,31 @@
+def check_has_solution(result: int, nums: list[int]) -> bool:
+    possible_answers = {nums[0]}
+
+    functions = [
+        lambda a, b: a + b,
+        lambda a, b: a * b,
+    ]
+
+    for num in nums[1:]:
+        new_possible_answers = set()
+        for possible_answer in possible_answers:
+            for function in functions:
+                answer = function(possible_answer, num)
+                new_possible_answers.add(answer)
+        possible_answers = new_possible_answers
+
+    return result in possible_answers
+
+
+def run(s: str) -> int:
+    lines = [line.strip() for line in s.strip().split("\n")]
+
+    total = 0
+    for line in lines:
+        result_part, nums_part = line.split(":")
+        result = int(result_part)
+        nums = [int(x) for x in nums_part.strip().split(" ")]
+        if check_has_solution(result, nums):
+            total += result
+
+    return total
---
+++ _2024/p07_test.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+from . import p07_a, p07_b
+
+
+def test_p07a_small():
+    s = """
+    190: 10 19
+    3267: 81 40 27
+    83: 17 5
+    156: 15 6
+    7290: 6 8 6 15
+    161011: 16 10 13
+    192: 17 8 14
+    21037: 9 7 18 13
+    292: 11 6 16 20"""
+
+    assert p07_a.run(s) == 3749
+
+
+def test_p07a():
+    s = (Path(__file__).parent / "p07_input.txt").read_text()
+
+    assert p07_a.run(s) == 6392012777720
+
+
+def test_p07b_small():
+    s = """
+    190: 10 19
+    3267: 81 40 27
+    83: 17 5
+    156: 15 6
+    7290: 6 8 6 15
+    161011: 16 10 13
+    192: 17 8 14
+    21037: 9 7 18 13
+    292: 11 6 16 20"""
+
+    assert p07_b.run(s) == 11387
+
+
+def test_p07b():
+    s = (Path(__file__).parent / "p07_input.txt").read_text()
+
+    assert p07_b.run(s) == 61561126043536
